cf05a5e2c5ad22c5d568979977375794a4dee058
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-01-19 12:20:57 +0100

    ajout de la gestion d'écran et mise en place de la gestion des données d'un projet

--- src/fMain.pas
+++ src/fMain.pas
@@ -16,7 +16,9 @@ uses
   uDMAppIcon,
   Olf.FMX.AboutDialog,
   FMX.Menus,
-  uProjectASSCG;
+  uProjectASSCG,
+  uProjectASSCGStores, FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts,
+  FMX.TabControl;
 
 type
   TfrmMain = class(TForm)
@@ -33,6 +35,21 @@ type
     mnuOutilsOptions: TMenuItem;
     mnuAide: TMenuItem;
     mnuAideAPropos: TMenuItem;
+    lBoutonsMenu: TLayout;
+    btnNouveau: TButton;
+    btnOuvrir: TButton;
+    lHomeScreen: TLayout;
+    lProjectScreen: TLayout;
+    tcProject: TTabControl;
+    tiProjectStores: TTabItem;
+    tiProjectImages: TTabItem;
+    tbFooter: TToolBar;
+    btnSave: TButton;
+    btnClose: TButton;
+    btnExport: TButton;
+    tiProjectLanguages: TTabItem;
+    tiBackground: TTabItem;
+    mnuOutilsReloadDBStores: TMenuItem;
     procedure OlfAboutDialog1URLClick(const AURL: string);
     procedure FormCreate(Sender: TObject);
     procedure mnuFichierQuitterClick(Sender: TObject);
@@ -43,17 +60,29 @@ type
     procedure mnuFichierOuvrirClick(Sender: TObject);
     procedure mnuOutilsOptionsClick(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
+    procedure btnExportClick(Sender: TObject);
+    procedure mnuOutilsReloadDBStoresClick(Sender: TObject);
   private
     FCurrentProject: TASSCGProject;
+    FDBStores: TASSCGDBStores;
+    FCurrentDisplay: TLayout;
     procedure SetCurrentProject(const Value: TASSCGProject);
     procedure InitFormTitle(AProject: TASSCGProject = nil);
     procedure InitProjectMenusOptions;
+    procedure SetDBStores(const Value: TASSCGDBStores);
+    procedure InitDBStore(AForceDownload: boolean = false);
+    procedure SetCurrentDisplay(const Value: TLayout);
     { Déclarations privées }
   protected
+    procedure GoToHomeScreen;
+    procedure GoToProjectScreen;
   public
     { Déclarations publiques }
     property CurrentProject: TASSCGProject read FCurrentProject
       write SetCurrentProject;
+    property DBStores: TASSCGDBStores read FDBStores write SetDBStores;
+    property CurrentDisplay: TLayout read FCurrentDisplay
+      write SetCurrentDisplay;
   end;
 
 var
@@ -67,11 +96,21 @@ uses
   System.Messaging,
   u_urlOpen,
   fOptions,
+  System.IOUtils,
   Olf.FMX.AboutDialogForm;
 
+procedure TfrmMain.btnExportClick(Sender: TObject);
+begin
+  // TODO : à compléter
+  showmessage('à faire');
+end;
+
 procedure TfrmMain.FormCreate(Sender: TObject);
+var
+  o: TFMXObject;
 begin
   InitFormTitle;
+  InitDBStore;
 
   // Menu de la fenêtre principale
 {$IFDEF MACOS}
@@ -125,11 +164,37 @@ begin
         InitFormTitle(msg.Project);
       end;
     end);
+
+  FCurrentDisplay := nil;
+  // Masque les TLayout servant d'écrans
+  for o in Children do
+    if (o is TLayout) and
+      (string((o as TLayout).Name).ToLower.EndsWith('screen')) then
+      (o as TLayout).visible := false;
+  // Bascule sur l'écran d'accueil
+  GoToHomeScreen;
 end;
 
 procedure TfrmMain.FormDestroy(Sender: TObject);
 begin
   CurrentProject.free;
+  DBStores.free;
+end;
+
+procedure TfrmMain.GoToHomeScreen;
+begin
+  CurrentDisplay := lHomeScreen;
+end;
+
+procedure TfrmMain.GoToProjectScreen;
+begin
+  if (DBStores.Count = 0) then
+    raise exception.create
+      ('No stores database. Please wait a minute or reload them from "Tools" options.');
+
+  tcProject.ActiveTab := tiProjectStores;
+  // TODO : à compléter
+  CurrentDisplay := lProjectScreen;
 end;
 
 procedure TfrmMain.mnuAideAProposClick(Sender: TObject);
@@ -178,6 +243,11 @@ begin
   end;
 end;
 
+procedure TfrmMain.mnuOutilsReloadDBStoresClick(Sender: TObject);
+begin
+  InitDBStore(true);
+end;
+
 procedure TfrmMain.mnuFichierQuitterClick(Sender: TObject);
 begin
   close;
@@ -188,19 +258,71 @@ begin
   url_Open_In_Browser(AURL);
 end;
 
+procedure TfrmMain.SetCurrentDisplay(const Value: TLayout);
+begin
+  if assigned(FCurrentDisplay) then
+    FCurrentDisplay.visible := false;
+
+  FCurrentDisplay := Value;
+
+  if assigned(FCurrentDisplay) then
+    FCurrentDisplay.visible := true;
+end;
+
 procedure TfrmMain.SetCurrentProject(const Value: TASSCGProject);
 begin
   if assigned(FCurrentProject) and (FCurrentProject <> Value) then
   begin
     if FCurrentProject.HasChanged then
     begin
-      // demander si on veut enregistrer
+      // TODO : demander si on veut enregistrer
     end;
     FCurrentProject.free;
   end;
 
   FCurrentProject := Value;
   InitProjectMenusOptions;
+end;
+
+procedure TfrmMain.SetDBStores(const Value: TASSCGDBStores);
+begin
+  FDBStores := Value;
+end;
+
+procedure TfrmMain.InitDBStore(AForceDownload: boolean);
+var
+  Folder, filename: string;
+begin
+  DBStores.free;
+  DBStores := TASSCGDBStores.create;
+
+{$IFDEF DEBUG}
+  Folder := tpath.combine(tpath.getdocumentspath, 'OlfSoftware-DEBUG',
+    'ASSCG-DEBUG');
+{$ELSE}
+  Folder := tpath.combine(tpath.GetHomePath, 'OlfSoftware', 'ASSCG');
+{$ENDIF}
+  if not TDirectory.Exists(Folder) then
+    TDirectory.CreateDirectory(Folder);
+
+{$IFDEF DEBUG}
+  filename := tpath.combine(Folder, 'asscg-debug.asscgstores');
+{$ELSE}
+  filename := tpath.combine(Folder, 'asscg.asscgstores');
+{$ENDIF}
+  if tfile.Exists(filename) and (not AForceDownload) then
+    DBStores.LoadFromFile(filename)
+  else
+    DBStores.LoadFromURL(CASSCGDBURL,
+      procedure
+      begin
+        DBStores.SaveToFile(filename);
+        showmessage('La base des magasins d''application a été mise à jour.');
+      end,
+      procedure
+      begin
+        showmessage('Erreur de téléchargement de la base des magasins.');
+      end);
 end;
 
 procedure TfrmMain.InitFormTitle(AProject: TASSCGProject);
@@ -214,9 +336,9 @@ begin
 
   if assigned(prj) then
     if prj.HasChanged then
-      caption := prj.name + '* - '
+      caption := prj.Name + '* - '
     else
-      caption := prj.name + ' - '
+      caption := prj.Name + ' - '
   else
     caption := '';
 
