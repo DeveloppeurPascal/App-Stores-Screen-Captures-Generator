8a613bacd0e1175fe12cd188a55c453acaf50f2d
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-10-16 15:16:23 +0200

    added copyright and timestamp comments

--- src/uConfig.pas
+++ src/uConfig.pas
@@ -1,3 +1,35 @@
+(* C2PP
+  ***************************************************************************
+
+  App Stores Screen Captures Generator
+
+  Copyright 2024-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://appstoresscreencapturesgenerator.olfsoftware.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/App-Stores-Screen-Captures-Generator
+
+  ***************************************************************************
+  File last update : 2025-10-16T10:42:30.918+02:00
+  Signature : 8bb8bde56c724d86c9d96e5087c04b7035464c51
+  ***************************************************************************
+*)
+
 unit uConfig;
 
 interface
